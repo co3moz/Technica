11b63096d705985a2f661bad98342a63463c844c
co3moz <co3moz@gmail.com>
2014-12-11 16:46:29 +0200

    Home allocated

--- Technica/DAL/TechnicaInitializer.cs
+++ Technica/DAL/TechnicaInitializer.cs
@@ -7,10 +7,14 @@ using Technica.Models;
 
 namespace Technica.DAL
 {
-    public class TechnicaInitializer : DropCreateDatabaseAlways<TechnicaContext>
+    public class TechnicaInitializer : DropCreateDatabaseIfModelChanges<TechnicaContext>
     {
         protected override void Seed(TechnicaContext context)
         {
+            Address address = new Address { Country = "Turkey", City = "Sakarya", ZipCode = "54100", UserID=0};
+            User user = new User { Email = "admin@admin.com", FirstName = "Admin", LastName = "istrator", LastAccessDate = DateTime.Now, RegistrationDate = DateTime.Now, Password = "admin", Phone = "555 444 33 22" };
+            context.Addresses.Add(address);
+            context.Users.Add(user);
         }
     }
 }
