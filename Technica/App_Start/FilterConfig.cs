f8fdf3819a7e06f8818537a917bd68bc932710d0
co3moz <co3moz@gmail.com>
2014-12-12 15:45:20 +0200

    Fix in Layout

--- Technica/App_Start/FilterConfig.cs
+++ Technica/App_Start/FilterConfig.cs
@@ -1,54 +1,14 @@
-﻿using System.Diagnostics;
-using System.Web;
-using System.Web.Mvc;
-using Technica.DAL;
-using Technica.Models;
-using System.Linq;
-using System.Collections.Generic;
+﻿using System.Web.Mvc;
+using Technica.App_Start;
 
 namespace Technica
 {
-    public class LanguageFilter : ActionFilterAttribute
-    {
-        private TechnicaContext db = new TechnicaContext();
-
-        public override void OnActionExecuting(ActionExecutingContext filterContext)
-        {
-            var session = filterContext.RequestContext.HttpContext.Session;
-            var viewbag = filterContext.Controller.ViewBag;
-
-            Debug.WriteLine("Request Comes");
-
-            viewbag.Currencies = db.Set<Currency>();
-            viewbag.Languages = db.Set<Language>();
-            viewbag.Categories = db.Set<Category>();
-
-            if (session["language"] == null)
-            {
-                session["language"] = db.Languages.First<Language>();
-            }
-
-            if (session["currency"] == null)
-            {
-                session["currency"] = db.Currencies.First<Currency>();
-            }
-
-            if (session["basket"] == null)
-            {
-                List<Basket> basket = new List<Basket>();
-                basket.Add(new Basket { product = db.Products.First<Product>(), quantity = 1 });
-                session["basket"] = basket;
-            }
-            base.OnActionExecuting(filterContext);
-        }
-    }
-
     public class FilterConfig
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            filters.Add(new LanguageFilter());
+            filters.Add(new GeneralFilter());
         }
     }
 }
---
+++ Technica/App_Start/GeneralFilter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Technica.DAL;
+using Technica.Models;
+
+namespace Technica.App_Start
+{
+    public class GeneralFilter : ActionFilterAttribute
+    {
+        private TechnicaContext db = new TechnicaContext();
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var session = filterContext.RequestContext.HttpContext.Session;
+            var viewbag = filterContext.Controller.ViewBag;
+
+            Debug.WriteLine("Request Comes");
+
+            viewbag.Currencies = db.Set<Currency>();
+            viewbag.Languages = db.Set<Language>();
+            viewbag.Categories = db.Set<Category>();
+
+            if (session["language"] == null)
+            {
+                session["language"] = db.Languages.First<Language>();
+            }
+
+            if (session["currency"] == null)
+            {
+                session["currency"] = db.Currencies.First<Currency>();
+            }
+
+            if (session["basket"] == null)
+            {
+                List<Basket> basket = new List<Basket>();
+                basket.Add(new Basket { product = db.Products.First<Product>(), quantity = 1 });
+                session["basket"] = basket;
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
