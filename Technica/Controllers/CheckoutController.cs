ac581ed7bd801f26f832fe6f5569ed1ae324f0a9
co3moz <co3moz@gmail.com>
2014-12-13 23:03:09 +0200

    Changess

--- Technica/Controllers/CheckoutController.cs
+++ Technica/Controllers/CheckoutController.cs
@@ -1,20 +1,71 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Technica.DAL;
+using Technica.Models;
 
 namespace Technica.Controllers
 {
     public class CheckoutController : Controller
     {
-        // GET: Checkout
+        private TechnicaContext db = new TechnicaContext();
+
+        [Route("Checkout")]
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("Checkout")]
+        public ActionResult IndexPost()
         {
             if (Session["user"] == null)
             {
                 return RedirectToAction("Index", "Home");
             }
+
+            User user = Session["user"] as User;
+            List<Basket> basket = Session["basket"] as List<Basket>;
+
+            Order order = new Order();
+            order.ActualAddress = Request["ActualAddress"];
+            order.City = Request["City"];
+            order.Country = Request["Country"];
+            order.Notes = Request["Notes"];
+            order.CurrencyID = (Session["currency"] as Currency).ID;
+            order.Date = DateTime.Now;
+            order.ZipCode = Request["ZipCode"];
+            order.UserID = user.ID;
+
+            List<Product> products = new List<Product>();
+
+            decimal money = 0;
+            foreach (Basket b in basket)
+            {
+                money += b.product.Price;
+                products.Add(b.product);
+            }
+
+            order.Count = products.Count;
+            order.Price = money;
+            order.JsonProducts = JsonConvert.SerializeObject(products);
+
+            if (ModelState.IsValid)
+            {
+                db.Orders.Add(order);
+                db.SaveChanges();
+                return RedirectToAction("Success");
+            }
+            return RedirectToAction("Index","Home");
+        }
+
+        public ActionResult Success()
+        {
             return View();
         }
     }
--- Technica/Models/Order.cs
+++ Technica/Models/Order.cs
@@ -14,12 +14,18 @@ namespace Technica.Models
         public int ID { get; set; }
         public int UserID { get; set; }
 
+        public int Count { get; set; }
         public decimal Price { get; set; }
         public int CurrencyID { get; set; }
 
-        public DateTime Date { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string ActualAddress { get; set; }
+        public string ZipCode { get; set; }
 
+        public DateTime Date { get; set; }
+        
         public string JsonProducts { get; set; }
-        public string JsonAdress { get; set; }
+        public string Notes { get; set; }
     }
 }
